716accef315faf5e38bd90634d85e4e42830dc9e
Shahrez19 <snjan19@bu.edu>
2015-07-14 13:08:40 -0400

    Starting Fragments

--- app/src/main/java/com/example/shahreznawfal/criminalintent/Crime.java
+++ app/src/main/java/com/example/shahreznawfal/criminalintent/Crime.java
@@ -1,18 +1,23 @@
 package com.example.shahreznawfal.criminalintent;
 
-        import java.util.UUID;
+import java.util.Date;
+import java.util.UUID;
 
 /**
  * Created by Shahrez Nawfal on 7/6/2015.
  */
+
 public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public Crime(){
         // Generate unique identifier
         mId = UUID.randomUUID();
+        mDate = new Date();
     }
 
     public UUID getId() {
@@ -25,5 +30,21 @@ public class Crime {
 
     public void setTitle(String title) {
         mTitle = title;
+    }
+
+    public Date getDate() {
+        return mDate;
+    }
+
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
     }
 }
